51ab17f927e9b9d200820ec444cc78ce420b3fe7
Dave Lewis <siu06dl@gmail.com>
2025-01-18 19:00:58 +0000

    fix: remove unnecessary component
    Not making a card game

--- Util/custom_shaders.gd
+++ Util/custom_shaders.gd
@@ -4,10 +4,16 @@ extends Node
 class_name CustomShaders
 
 static var _simpleColorShader: Resource = load("res://Util/Shaders/simple_color.gdshader")
+static var _eyeShader: Resource = load("res://Util/Shaders/eye.gdshader")
 
 static func GetSimpleColorShader() -> ShaderMaterial:
-	print("loading")
-	var shader: Resource = _simpleColorShader.duplicate(true)
+	return _get_generic_shader(_simpleColorShader)
+
+static func GetEyeShader() -> ShaderMaterial:
+	return _get_generic_shader(_eyeShader)
+
+static func _get_generic_shader(res: Resource) -> ShaderMaterial:
+	var shader: Resource = res.duplicate(true)
 	
 	var material :ShaderMaterial = ShaderMaterial.new()
 	material.shader = shader
