dcd403e8fe67d391bfd24810ec9bfc733dcd38d2
Dave Lewis <siu06dl@gmail.com>
2025-01-19 21:11:02 +0000

    feat: game over menu

---
+++ UI/Components/game_over.gd
@@ -0,0 +1,11 @@
+extends CenterContainer
+
+
+func _on_new_game_button_pressed():
+	Global.gameController.NewGame()
+
+
+func _on_quit_button_pressed():
+	Global.gameController._mainMenu.continueButton.disabled = true
+	Global.gameController.ResetGame()
+	Global.gameController.ShowMainMenu()
